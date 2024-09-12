534b57fab26647a2def7f3b9a01318a15d93fb6b
alexcyrus <alex@alexcyrus.com>
2024-09-12 17:43:25 -0500

    style footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,10 @@
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="w-full pt-20 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img src="/footer-grid.svg" alt="grid" className="w-full h-full opacity-50" />
+      </div>
+    </footer>
   )
 }
 
