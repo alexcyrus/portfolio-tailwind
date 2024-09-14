e0c03666c0c2e62ffd56fdf647b4a57ae58ca8d3
alexcyrus <alex@alexcyrus.com>
2024-09-14 13:31:11 -0500

    edit floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
+"use client";
+import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Footer from "@/components/Footer";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome /> }
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
